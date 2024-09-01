07dc7749325be9d4282a19fbcdc2c8dab6d162aa
Reiy31497 <reiybeaudoin@unomaha.edu>
2024-09-01 22:13:44 +0000

    Finished MadLib.py

--- MadLib.py
+++ MadLib.py
@@ -1,13 +1,19 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name: Reiy
+#Date: 9/1/24
+#Assignment: Lab 1
 
 def main():
   print("Madlib")
   #Ask user for words
-
+  noun = input("Enter a noun: ")
+  adjective = input("Enter an adjective: ")
+  feeling = input("Enter a feeling: ")
+  bodypart = input("Enter a bodypart: ")
+  verb1 = input("Enter a verb: ")
+  verb2 = input("Enter another verb: ")
   #Print the story with the user supplied words.
+  print("Today, I go to " +noun+ " class. It is very " +adjective+ ". I start to get " +feeling+ ". My " +bodypart+ " gets tired. After class, I start to " +verb1+ ". Time to " +verb2+ "!")
 
 
 
